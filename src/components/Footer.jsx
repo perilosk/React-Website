3c331c10975391b7a29fcb98ba52fe30afe65ddd
Pichayachai <perilosk@gmail.com>
2026-03-24 11:03:36 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className='bg-sky-300 p-5 text-center dark:bg-zinc-900 dark:text-white'>
-        <p>Copyright 2025. ReactDev All Rights Reserved.</p>
+        <p>Copyright 2026. Perilos All Rights Reserved.</p>
     </footer>
   )
 }
